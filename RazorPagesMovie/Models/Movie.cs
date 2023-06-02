52c03bbc5ced0e670c52fab62612f0e947e8d93c
dariorodt <dariorodt@gmail.com>
2023-06-02 16:50:39 -0400

    Agregar archivos de proyecto.

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        // Important!
+        // When using a default value in a [Required] field never, ever,
+        // use null coalescing operator in the property type.
+        // Example:
+        // Do:    public string Title { get; set; } = string.Empty;
+        // Don't: public string? Title { get; set; } = string.Empty;
+
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [StringLength(30)]
+        [Required]
+        public string Genre { get; set; } = string.Empty;
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public Decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
+        public string Rating { get; set; } = string.Empty;
+    }
+}
